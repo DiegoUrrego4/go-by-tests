af3f3921dae8603ebbf01f460cba97c92b68fe48
Diego Urrego <diegourrg@gmail.com>
2024-12-24 16:11:19 -0500

    feat: hello-world app and tests implemented

---
+++ hello-world/hello_test.go
@@ -0,0 +1,45 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		// Arrange
+		want := "Hello, Chris"
+
+		// Act
+		got := Hello("Chris", "")
+
+		// Assert
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say, 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should say hello in Spanish", func(t *testing.T) {
+		got := Hello("Elias", "Spanish")
+		want := "Hola, Elias"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("should say hello in French", func(t *testing.T) {
+		got := Hello("Elias", "French")
+		want := "Bonjour, Elias"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
